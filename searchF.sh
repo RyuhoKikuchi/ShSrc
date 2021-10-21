ccac8c57ceace7df9ebbae666eb823d31183e409
Ryuho Kikuchi <agemarudon1@gmail.com>
2021-10-20 18:37:17 -0700

    My first shell script file ディレクトリ配下に存在する全てのJSファイルに含まれる関数探索を行う簡易的なファイル 主に練習用である。

---
+++ searchF.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+
+#  for i in $(find ./ | grep \.js | grep -v "jquery"); do bat $i | pcre2grep -Mr 'function[\s\S]*?\n}\n'; echo "}\n"; done
+
+# This file was meant to extract functions in all JS files in child directories
+# This searching Algorithm needs better version
+# Hint: quatation(including comments), json, return.... the list goes on
+
+# The first for_loop statement is searching all Paths to the files
+# li1 var is finding index for the beginning line of functions
+# li2 is for the end of the function
+# The second for_loop inside is extracting the code for the function using specified the numbers of lines
+
+
+for x in $(find ./ | grep "\.js" | grep -v "jquery"); do
+    li1=(${$(cat $x | grep -n "function" | grep "(" | grep -v "//" | awk -F ":" '{print $1}')//\\n/ })
+    li2=(${$(cat $x | grep -n "}" | grep -v "    " | awk -F ":" '{print $1}')//\\n/ })
+    echo "$x \n\n${li1[@]}\n${li2[@]}\n\n"
+    for i in $(seq 1 ${#li1[@]}); do
+        cat $x | sed -n "${li1[$i]},${li2[$i]}p" | bat -l JavaScript
+    done
+done
